d3d0d807bb64c94ea2436ce95f37355a2798d85e
tharri6 <timothyharrris@186.255.21.198.tigernet.wifi.dyn.clemson.edu>
2017-03-09 16:21:19 -0500

    Timothy Harris uptaded files

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
-# Node.js Hello World Sample
+# Node.js Starter Overview
 
-This application demonstrates a simple, reusable Node.js web application based on the Express framework.
-
-[![Deploy to Bluemix](https://bluemix.net/deploy/button.png)](https://bluemix.net/deploy?repository=https://github.com/IBM-Bluemix/nodejs-helloworld)
+The Node.js Starter demonstrates a simple, reusable Node.js web application based on the Express framework.
 
 ## Run the app locally
 
 1. [Install Node.js][]
-+ cd into this project's root directory
-+ Run `npm install` to install the app's dependencies
-+ Run `npm start` to start the app
-+ Access the running app in a browser at <http://localhost:6001>
+2. Download and extract the starter code from the Bluemix UI
+3. cd into the app directory
+4. Run `npm install` to install the app's dependencies
+5. Run `npm start` to start the app
+6. Access the running app in a browser at http://localhost:6001
 
 [Install Node.js]: https://nodejs.org/en/download/
--- app.js
+++ app.js
@@ -7,7 +7,9 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+  request = require('request'),
 
+_ = require('lodash');
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
@@ -20,6 +22,34 @@ app.use(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
+
+
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      latitude:req.query.latitude,
+      longitude:req.query.longitude,
+	  startDate:req.query.startDate,
+	  endDate:req.query.endDate
+     };
+                     
+      var callURL = "https://54549c0b-9018-44dd-8d78-f0dd1d628a98:DO2cRAPAFa@twcservice.mybluemix.net/api/weather/v1/geocode/"+response.latitude+"/"+response.longitude+"/almanac/daily.json?units=e&start="+response.startDate+"&end="+response.endDate
+
+      request.get(callURL, {
+        json: true
+      },
+      function (error, response, body) {
+		if(error){
+			console.log(error);
+		} else {
+			//var meta = body["metadata"];
+			//var id = meta["transaction_id"];
+			res.send(body);
+		}
+
+      });
+})
+
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
